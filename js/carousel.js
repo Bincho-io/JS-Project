a66227ba3b2d25c592a7334c5c42b59c35718526
Bincho-io <micsoroft+bincho@gmail.com>
2026-07-21 20:07:34 -0400

    Made slides

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,11 @@
 "use strict";
 {
+  const next = document.getElementById('next');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
 
+  next.addEventListener('click', () => {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+  });
 }
